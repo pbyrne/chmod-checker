d141d6d42b0280a816e1271c8693205d2dfad58f
Patrick Byrne <code@patrickbyrne.net>
2014-05-26 16:53:49 -0500

    Move static descriptions from view to model
    Then we need to make the model dynamic and the views will Just Work™.

--- app/models/chmod.rb
+++ app/models/chmod.rb
@@ -1,3 +1,19 @@
 class Chmod
   include ActiveModel::Conversion
+
+  def mode
+    "0644" # FIXME make this dynamic
+  end
+
+  def user_description
+    "read and write" # FIXME make this dynamic
+  end
+
+  def group_description
+    "read" # FIXME make this dynamic
+  end
+
+  def other_description
+    "read" # FIXME make this dynamic
+  end
 end
